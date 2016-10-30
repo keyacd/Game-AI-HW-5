6bbb7a29fcdecbfd7f3a4e251a9c0676fc0aa53c
Valectar <nick.colclasure@me.com>
2016-10-30 13:08:36 -0400

    Add edge cost function and fix documentation wording / typos

--- Graph_prototype.py
+++ Graph_prototype.py
@@ -3,10 +3,13 @@ class Graph(object):
 	def __init__(self):
 		super(Graph, self).__init__()
 	def getAdjacent(self, pos):
-		"""Get all adjacent nodes in the graph"""
+		"""Get all nodes adjacent to pos in the graph"""
 		pass
 	def getHeuristicDist(self, start, end):
-		"""Get the heuristic distance between tow nodes start and end in the graph"""
+		"""Get the heuristic distance between the nodes start and end in the graph"""
+		pass
+	def getEdgeCost(self, n1, n2):
+		"""Get edge cost from node n1 to n2"""
 		pass
 	def getPositions(self):
 		"""Return a list of all nodes in the graph"""
