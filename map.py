608750806fd65e409dc6eb34159407b0b2862d23
Darien Keyack <darienkeyack@gmail.com>
2016-10-30 09:35:56 -0400

    added Tile class
    also started adding a dropdown but I realized I don't actually know how
    paintEvent works sooooo

--- map.py
+++ map.py
@@ -1,6 +1,23 @@
 import sys
 from fractions import gcd
-from PyQt4 import QtGui, QtCore
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+class Tile():
+	def __init__(self, size, tile):
+		tile = "".join(tile)
+		clear = tile.count(".")
+		if clear == size * size:
+			self.open = True
+		else:
+			self.open = False
+		self.explored = False
+		self.active = False
+	def activate(self):
+		self.active = True
+		self.explored = True
+	def deactivate(self):
+		self.active = False
 
 def FormatInfo(raw):
 	info = raw.pop(0)
@@ -53,17 +70,11 @@ def MakeTiles(filename, size):
 			tile = []
 			for c in col:
 				tile.append(c[x * size: x * size + size])
-			tile = "".join(tile)
-			clear = tile.count(".")
-			if clear == size * size:
-				tile = "."
-			else:
-				tile = "X"
-			row.append(tile)
+			row.append(Tile(size, tile))
 		tiles.append(row)
 	return tiles
 
-class window(QtGui.QWidget):
+class window(QWidget):
 	def __init__(self):
 		super(window, self).__init__()
 		self.size = 2
@@ -73,33 +84,43 @@ class window(QtGui.QWidget):
 		for m in self.maps:
 			new = MakeTiles(m, self.size)
 			self.tiles.append(MakeTiles(m, self.size))
+		self.iMap = 0
+		layout = QHBoxLayout()
+		self.cb = QComboBox()
+		self.cb.addItems(self.maps)
+		self.cb.currentIndexChanged.connect(self.comboChange)
+		#layout.addWidget(self.cb)
+		self.setLayout(layout)
 		self.initUI()
+
+	def comboChange(self,i):
+		self.iMap = i
         
 	def initUI(self):      
-		self.setWindowState(QtCore.Qt.WindowMaximized)
-		self.setWindowTitle('Tile Size: ' + str(self.size) + ", Map: " + self.maps[0] + " " + str(len(self.tiles[0][0])) + "x" + str(len(self.tiles[0])))
+		self.setWindowState(Qt.WindowMaximized)
+		self.setWindowTitle('Tile Size: ' + str(self.size) + ", Map: " + self.maps[self.iMap] + " " + str(len(self.tiles[0][0])) + "x" + str(len(self.tiles[0])))
 		self.show()
 
 	def paintEvent(self, e):
-		qp = QtGui.QPainter()
+		qp = QPainter()
 		qp.begin(self)
 		self.drawRectangles(qp)
 		qp.end()
 	    
 	def drawRectangles(self, qp):  
-		color = QtGui.QColor(0, 0, 0)
+		color = QColor(0, 0, 0)
 		color.setNamedColor('#d4d4d4')
 		qp.setPen(color)
 
-		for y in range(0, len(self.tiles[0])):
-			for x in range(0, len(self.tiles[0][0])):
-				if self.tiles[0][y][x] == "X":
-					qp.setBrush(QtGui.QColor(25, 80, 90, 200))
+		for y in range(0, len(self.tiles[self.iMap])):
+			for x in range(0, len(self.tiles[self.iMap][0])):
+				if self.tiles[self.iMap][y][x].open:
+					qp.setBrush(QColor(200, 0, 0, 0))
 				else:
-					qp.setBrush(QtGui.QColor(200, 0, 0, 0))
-				qp.drawRect(x * sizeDraw, y * sizeDraw, 5 * sizeDraw, 5 * sizeDraw)
+					qp.setBrush(QColor(25, 80, 90, 200))
+				qp.drawRect(x * self.sizeDraw, y * self.sizeDraw, self.sizeDraw, self.sizeDraw)
 
 if __name__ == "__main__":
-	app = QtGui.QApplication(sys.argv)
+	app = QApplication(sys.argv)
 	ex = window()
 	sys.exit(app.exec_())
